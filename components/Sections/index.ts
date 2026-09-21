d9a4447f72c5654f9df837c5831173124a8fb858
Janith Chathuranga <janithsamarasinghe1999@gmail.com>
2026-09-21 11:30:55 +0530

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

--- components/Sections/index.ts
+++
@@ -1,5 +0,0 @@
-export { About } from "./About";
-export { Experience } from "./Experience";
-export { Projects } from "./Projects";
-export { Contact } from "./Contact";
-export { Hero } from "./Hero";
